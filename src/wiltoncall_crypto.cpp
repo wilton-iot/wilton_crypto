373081c5358c0638e074f7ca2cbcca0e95b00fe8
alex <alex@staticlibs.net>
2017-12-03 19:45:22 +0000

    initial commit

---
+++ src/wiltoncall_crypto.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   wiltoncall_crypto.cpp
+ * Author: alex
+ *
+ * Created on December 3, 2017, 6:40 PM
+ */
+
+#include "staticlib/support.hpp"
+
+#include "wilton/support/buffer.hpp"
+#include "wilton/support/exception.hpp"
+#include "wilton/support/registrar.hpp"
+
+extern "C" char* wilton_module_init() {
+    try {
+        // register calls
+        return nullptr;
+    } catch (const std::exception& e) {
+        return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
+    }
+}
